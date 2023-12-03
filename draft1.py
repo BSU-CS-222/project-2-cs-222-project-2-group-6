f9ce940924719b8557c645bfe96424bb6d7944af
chitk <chitkoko7214084@gmail.com>
2023-12-03 01:45:34 -0500

    I added pretty table module and backtrack ALG

--- draft1.py
+++ draft1.py
@@ -1,90 +1,146 @@
+from prettytable import PrettyTable
+
 class Student:
     def __init__(self, student_id, max_credit_hours):
         self.student_id = student_id
         self.max_credit_hours = max_credit_hours
-        self.schedule = [] 
+        self.schedule = []
 
-    def add_course(self, course_info):
-        try:
-            course_code = course_info
-            if course_code in self.courses_dict:
-                available_classes = self.courses_dict[course_code]
-                print(f"Available Sections for {course_code}:")
+    def add_course(self, course_code):
+      try:
+        if course_code in self.courses_dict:
+            available_classes = self.courses_dict[course_code]
+
+            available_classes_table = PrettyTable(["Section", "Days", "Start-Time", "End-Time", "Credits"])
+            for i, class_info in enumerate(available_classes):
+                available_classes_table.add_row([f"{i}. {class_info[0]}", class_info[1], class_info[2], class_info[3], class_info[4]])
+
+                print(available_classes_table)  # Display available classes table
+
                 print("Choose by inputting number, where '0' is the course at the top of the list.")
 
-                for class_info in available_classes:
-                    print(class_info)
+            class_choice = int(input("Enter the number of the section you want to register for: "))
+            selected_section = available_classes[class_choice]
 
-                class_choice = int(input("Enter the number of the section you want to register for: "))
-                selected_section = available_classes[class_choice]
+            # Extract relevant information from the selected section
+            section, days, start_time, end_time, credits = map(str.strip, selected_section)
+            formatted_course_info = f"{course_code},{section},{days},{start_time},{end_time},{credits}"
 
-                section, days, start_time, end_time, credits = selected_section
-                formatted_course_info = f"{course_code},{section},{days},{start_time},{end_time},{credits}"
-
-                if not self.has_time_conflict(formatted_course_info):
-                    self.schedule.append(formatted_course_info) #adds courses to your schedule
-                    return "Successfully added."
-                else:
-                    print("Time conflict with an existing course.")
-
+            if not self.has_time_conflict(formatted_course_info):
+                self.schedule.append(formatted_course_info)  # adds courses to your schedule
+                return "Successfully added."  
             else:
-                return f"Course code {course_code} not found in available courses."
+                print("Time conflict with an existing course. You have been added this course.")
+                return "Please try to add different course, no conflict time with your existing courses."
 
-        except ValueError:
-            return "Time conflict with an existing course, please try again."
+        else:
+            return f"Course code {course_code} not found in available courses."
+
+      except ValueError:
+        return "Invalid input. Please try again."
+
+    def resolve_time_conflict(self, new_course_details):
+        for existing_course_index, existing_course in enumerate(self.schedule):
+            # Check for overlapping days and time
+            if existing_course.split(",")[2] == new_course_details.split(",")[2] and \
+               max(existing_course.split(",")[3], new_course_details.split(",")[3]) < min(existing_course.split(",")[4], new_course_details.split(",")[4]):
+
+                # Attempt to resolve conflict by swapping courses using backtracking
+                if self.try_swap_courses(existing_course_index, new_course_details):
+                    print("Time conflict resolved by swapping courses.")
+                    return True
+
+        # Unable to resolve conflict
+        return False
+
+    def try_swap_courses(self, existing_course_index, new_course_details):
+        # Attempt to swap the conflicting courses using backtracking
+        temp_schedule = self.schedule.copy()
+        temp_schedule[existing_course_index], temp_schedule[-1] = temp_schedule[-1], temp_schedule[existing_course_index]
+
+        if self.backtrack_resolve_conflicts(temp_schedule):
+            self.schedule = temp_schedule[:-1]  # Remove the temporarily added course
+            return True
+        else:
+            return False
+
+    def backtrack_resolve_conflicts(self, schedule):
+        # Backtracking algorithm to resolve conflicts in the schedule
+
+        # Base case: If there are no more conflicts, return True
+        if not self.has_conflicts(schedule):
+            return True
+
+        # Try swapping conflicting courses and recursively backtrack
+        for i in range(len(schedule) - 1):
+            if self.has_time_conflict(schedule[i]):
+                temp_schedule = schedule.copy()
+                temp_schedule[i], temp_schedule[-1] = temp_schedule[-1], temp_schedule[i]
+
+                # Recursively check if the modified schedule is conflict-free
+                if self.backtrack_resolve_conflicts(temp_schedule):
+                    schedule[:] = temp_schedule
+                    return True
+
+        # If no valid swap is found, return False
+        return False
+
+    def has_conflicts(self, schedule):
+        # Check if there are any conflicts in the schedule
+        for i in range(len(schedule) - 1):
+            if self.has_time_conflict(schedule[i]):
+                return True
+        return False
 
     def has_time_conflict(self, new_course_details):
         for existing_course in self.schedule:
             # Check for overlapping days and time
-            if existing_course[2] == new_course_details[2] and \
-               max(existing_course[3], new_course_details[3]) < min(existing_course[4], new_course_details[4]):
+            if existing_course.split(",")[2] == new_course_details.split(",")[2] and \
+               max(existing_course.split(",")[3], new_course_details.split(",")[3]) < min(existing_course.split(",")[4], new_course_details.split(",")[4]):
                 return True
         return False
 
-    
     def view_schedule(self):
         if not self.schedule:
             return "Your schedule is empty."
         else:
-            schedule_header = "Class Schedule:\n"
-            schedule_table = "X----------------------------------------------------X\n"
-            schedule_table += "|Course|Section|Days|Start-Time|End-Time|Credits\n"
-            schedule_table += "X---------------------------------------------------X\n"
+            schedule_table = PrettyTable(["Course", "Section", "Days", "Start-Time", "End-Time", "Credits"])
 
             for course_str in self.schedule:
-                # No need to split course_str since it's already a string
-                schedule_table += f"| {course_str}\n"
+                course_info = course_str.split(",")
+                schedule_table.add_row(course_info)
 
-            schedule_table += "+----------------------------------------------+"
-            return schedule_header + schedule_table
-        
+            return f"Class Schedule:\n{schedule_table}"
+
     def read_courses_from_file(self, file_path):
-         with open(file_path, 'r') as file:   #stores info from txt file to dict.
+        with open(file_path, 'r') as file:  # stores info from txt file to dict.
             self.courses_dict = {}
             for line in file:
                 course_data = line.strip().split(',')
-                
+
                 course_code = course_data[0]
-                
+
                 course_details = course_data[1:]
-                
-                if course_code not in self.courses_dict: #makes sure it prints every course from txt file
-                                                         #not just 1 of each category of course code
+
+                if course_code not in self.courses_dict:
+                    # makes sure it prints every course from txt file
+                    # not just 1 of each category of course code
                     self.courses_dict[course_code] = []
 
                 self.courses_dict[course_code].append(course_details)
 
-            return self.courses_dict
+        return self.courses_dict
 
     def view_available_courses(self):
-        print("Available Courses:")
+        available_courses_table = PrettyTable(["Course Code", "Section", "Days", "Start-Time", "End-Time", "Credits"])
         for course_code, course_details_list in self.courses_dict.items():
             for course_details in course_details_list:
-                print(f"{course_code}: {course_details}")
+                available_courses_table.add_row([course_code] + course_details)
 
-    
+        print("Available Courses:")
+        print(available_courses_table)
+
     def main(self):
-        
         while True:
             print('''\nx------------------------------------------x
 |              Class Schedule              |
@@ -95,15 +151,13 @@ x------------------------------------------x''')
             print("4. Clear Schedule")
             print("5. Exit")
 
-            
             choice = input("Please make your choice (1 to 5): ")
 
             if choice == '1':
                 self.read_courses_from_file("classes.txt")
-                student_instance.read_courses_from_file("classes.txt")
                 num_courses = int(input("Enter the number of courses you'd like to register for: "))
                 for i in range(num_courses):
-                    course_code = input("Enter the course code (e.g., CS120, CS121, CS122, CS416, MATH181, CS418, CS239): ")
+                    course_code = input("Enter the course code (e.g., MATH166, CS222, CS120, CS124, CS121, CS230, MATH165): ")
                     result = self.add_course(course_code)
                     print(result)
             elif choice == '2':
@@ -118,7 +172,7 @@ x------------------------------------------x''')
                 print("Good luck with your classes!")
                 break
             else:
-                print("Please only choose 1 to 6, try again!")
+                print("Please only choose 1 to 5, try again!")
 
 # Create a student instance
 student_instance = Student("12345", 15)
